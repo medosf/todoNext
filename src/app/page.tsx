bf2031c3c062bb7fff12f10b427f0fe79595a777
medosf <43324373+medosf@users.noreply.github.com>
2024-06-09 14:28:27 -0400

    adding todo UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,57 @@
+'use client';
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  // need to add context
+  // create hooks for the context
+
+  const [todos, setTodos] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  
+
+  const handleCheckboxChange = (id: number, isComplete: boolean) => {
+    setTodos((prevTodos: any) => {
+      return prevTodos.map((todo: any) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isComplete,
+          };
+        }
+        return todo;
+      });
+    });
+
+    //Todo add api call to update the todo
+    // need to implement the call in the backend first
+
+  }
+
+
+ 
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchTodos = async () => {
+      try {
+        setError(false)
+        const response = await fetch('https://todoapp101.azurewebsites.net/todo');
+        const data = await response.json();
+          console.log("data >>>>", data);
+        setTodos(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching todos:', error);
+        setIsLoading(false);
+        setError(true);
+      }
+    };
+     fetchTodos();
+
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
@@ -16,6 +67,23 @@ export default function Home() {
               height={24}
               priority
             />
+        </div>
+
+
+        <div>
+          {isLoading && <p>Loading...</p>}
+          {error && <p>Error fetching todos, please try again later</p>}
+          <ul>
+            {todos.map((todo:any) => (
+              
+              <li key={todo.id} className="text-black">
+                <div className="flex pt-2">
+                <p className="w-4/5">{todo.name}</p>
+                <input className="w-1/5" type="checkbox" checked={todo.isComplete} onClick={()=>handleCheckboxChange(todo.id, !todo.isComplete)} />
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
  

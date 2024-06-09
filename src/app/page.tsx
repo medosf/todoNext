08d4ffdec40fb4f874c4bfe198ad787dd1ec0cc3
medosf <43324373+medosf@users.noreply.github.com>
2024-06-09 17:33:05 -0400

    update deployment - UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,55 +9,44 @@ export default function Home() {
   const [todos, setTodos] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchTodos = async () => {
+    try {
+      setError(false)
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URI}/todo`);
+      const data = await response.json();
+      setTodos(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+      setIsLoading(false);
+      setError(true);
+    }
+  };
   
 
-  const handleCheckboxChange = (id: number, isComplete: boolean) => {
-    setTodos((prevTodos: any) => {
-      return prevTodos.map((todo: any) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            isComplete,
-          };
-        }
-        return todo;
-      });
-    });
+  const handleDelete = async (id: number) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URI}/todo/${id}`, { 
+    method: 'DELETE',
+    })
 
-    //Todo add api call to update the todo
-    // need to implement the call in the backend first
-
+  if (res.status === 200) {
+    fetchTodos();
   }
 
-
- 
+ }
 
   useEffect(() => {
     setIsLoading(true);
-    const fetchTodos = async () => {
-      try {
-        setError(false)
-        const response = await fetch('https://todoapp101.azurewebsites.net/todo');
-        const data = await response.json();
-          console.log("data >>>>", data);
-        setTodos(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching todos:', error);
-        setIsLoading(false);
-        setError(true);
-      }
-    };
-     fetchTodos();
+
+      fetchTodos();
 
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
+    <main className="flex min-h-100 min-w-60 flex-col items-center gap-10 p-12">
       
       <h2>Welcome to my todo app</h2>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
      
             <Image
               src="/todo.svg"
@@ -67,10 +56,8 @@ export default function Home() {
               height={24}
               priority
             />
-        </div>
 
 
-        <div>
           {isLoading && <p>Loading...</p>}
           {error && <p>Error fetching todos, please try again later</p>}
           <ul>
@@ -79,13 +66,22 @@ export default function Home() {
               <li key={todo.id} className="text-black">
                 <div className="flex pt-2">
                 <p className="w-4/5">{todo.name}</p>
-                <input className="w-1/5" type="checkbox" checked={todo.isComplete} onClick={()=>handleCheckboxChange(todo.id, !todo.isComplete)} />
+                <button className="w-1/5" onClick={()=>handleDelete(todo.id)}>
+                  <Image
+                    src="/delete.svg"
+                    alt="delete icon"
+                    className="dark:invert"
+                    width={24}
+                    height={24}
+                  priority
+                />
+                </button>
+            
                 </div>
               </li>
             ))}
           </ul>
-        </div>
-      </div>
+        <button className="bg-black text-white rounded-md p-2 px-12">Add Todo</button>
  
     </main>
   );
